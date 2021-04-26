45642e52bc0e2f3570948d50b6e2d2a627f9896a
Ioannis Koinaris <ikoinaris@mgm-edv.de>
2021-04-26 16:52:42 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Animal {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    speak() {
+        console.log('I am ', this.name, ' and I am ', this.age, ' years old.');
+    }
+}
+
+const animal1 = new Animal('Simba', 4);
+animal1.speak();
+
