f05855cc63d4dc94e910904fc1642775a455ea42
Ioannis Koinaris <ikoinaris@mgm-edv.de>
2021-04-27 10:00:26 +0200

    displayBio added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
+
 class App extends Component{
+    constructor() {
+        super();
+        this.state = { displayBio: true };
+    }
     render() {
         return (
             <div>
                 <h1>Hello !</h1>
                 <p>My name is Yannis</p>
                 <p>I am an IT consultant</p>
+                {
+                    this.state.displayBio ? (
+                        <div>
+                            <p>I live in Munich and code every day.</p>
+                            <p>My favorite language is Java and I think React.js is awesome.</p>
+                            <p>Besides coding, I also love movies and music.</p>
+                        </div>
+                    ) : null
+                }
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -7,33 +7,3 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-class Animal {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    speak() {
-        console.log('I am ', this.name, ' and I am ', this.age, ' years old.');
-    }
-}
-
-const animal1 = new Animal('Simba', 4);
-animal1.speak();
-
-class Lion extends Animal {
-    constructor(name, age, furColor, speed) {
-        super(name, age);
-        this.furColor = furColor;
-        this.speed = speed;
-    }
-
-    roar() {
-        console.log('ROOOAR! I have ', this.furColor, ' fur and I can run ', this.speed, ' miles an hour.');
-    }
-}
-
-const lion1 = new Lion('Mufasa', 20, 'golden', '25');
-lion1.speak();
-lion1.roar();
-
