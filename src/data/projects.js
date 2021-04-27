55a89634a16a972bdb5f7b6a7f1f40381eaf02d7
Ioannis Koinaris <ikoinaris@mgm-edv.de>
2021-04-27 11:37:03 +0200

    Project component and props

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,21 +7,21 @@ const PROJECTS = [
         id: 1,
         title: 'Example SpringBoot Application',
         description: 'A Gradle project using Apache Kafka with SpringBoot',
-        link: 'https://github.com/ikoinaris/library',
+        url: 'https://github.com/ikoinaris/library',
         image: project1
     },
     {
         id: 2,
         title: 'MEAN Application',
         description: 'Project based on MEAN Stack (Angular, NodeJS, Express and MongoDB)',
-        link: 'https://github.com/ikoinaris/MEANProject',
+        url: 'https://github.com/ikoinaris/MEANProject',
         image: project2
     },
     {
         id: 3,
         title: 'Hotel Application',
         description: 'This Project has been created using Spring Boot and MongoDb.',
-        link: 'https://github.com/ikoinaris/Hotel-Project',
+        url: 'https://github.com/ikoinaris/Hotel-Project',
         image: project3
     }
 ];
