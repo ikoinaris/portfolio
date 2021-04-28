f00a132d96fc8e7a13bb76a82a4c125c8f4acebb
Ioannis Koinaris <ikoinaris@mgm-edv.de>
2021-04-28 09:22:49 +0200

    router and history attached

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Switch, Route} from 'react-router-dom';
 import App from "./components/App";
+import Jokes from "./components/Jokes";
 import './index.css';
+import {createBrowserHistory} from "history";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+    <Router history={createBrowserHistory()}>
+        <Switch>
+            <Route exact path='/' component={App}></Route>
+            <Route path='/jokes' component={Jokes}></Route>
+        </Switch>
+    </Router>,
+    document.getElementById('root')
+);
